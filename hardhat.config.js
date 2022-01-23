b8fb31e677c5aee5e0e3014cbd79ba81721f28d9
gourav <gvirus21yt@gmail.com>
2022-01-23 20:34:25 +0530

    updated hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const { MAINNET_API_URL, MUMBAI_API_URL } = process.env;
+const fs = require("fs");
+
+const PRIVATE_KEY = fs.readFileSync(".secret").toString()
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: MUMBAI_API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    mainnet: {
+      url: MAINNET_API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.4",
+};
